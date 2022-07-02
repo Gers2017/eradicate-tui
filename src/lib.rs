a72c0af3659594588d5df6b0789dd856a4ca7d18
Gers2017 <Gers2017@outlook.com>
2022-07-01 21:18:43 -0600

    Initial commit - Rusty dusty

---
+++ src/lib.rs
@@ -0,0 +1,189 @@
+use glob::{glob_with, MatchOptions};
+use std::{error::Error, fs, path::PathBuf};
+use tui::widgets::ListState;
+
+pub enum InputMode {
+    Normal,
+    Insert,
+}
+
+pub type ErrorBox = Box<dyn Error>;
+
+pub struct App {
+    pub list: StatefulList<PathEntry>,
+    pub curret_input: String,
+    pub input_mode: InputMode,
+    pub pattern: String,
+    glob_options: MatchOptions,
+}
+
+impl App {
+    pub fn new() -> Self {
+        App {
+            list: StatefulList::new(),
+            curret_input: String::new(),
+            input_mode: InputMode::Normal,
+            pattern: String::new(),
+            glob_options: MatchOptions::new(),
+        }
+    }
+
+    pub fn on_tick(&self) {
+        // todo!("update me!")
+    }
+
+    pub fn set_pattern(&mut self, pattern: &str) -> Result<(), ErrorBox> {
+        self.pattern = pattern.to_string();
+        let entries = self.search_with_pattern()?;
+        self.update_list(entries);
+        Ok(())
+    }
+
+    pub fn is_case_sensitive(&self) -> bool {
+        self.glob_options.case_sensitive
+    }
+
+    pub fn toggle_case_sensitive(&mut self) {
+        self.glob_options.case_sensitive = !self.glob_options.case_sensitive;
+    }
+
+    fn search_with_pattern(&self) -> Result<Vec<PathEntry>, ErrorBox> {
+        let entries: Vec<PathEntry> = glob_with(&self.pattern.as_ref(), self.glob_options)?
+            .filter_map(Result::ok)
+            .map(PathEntry::new)
+            .collect();
+        Ok(entries)
+    }
+
+    fn update_list(&mut self, entries: Vec<PathEntry>) {
+        self.list = StatefulList::with_items(entries);
+    }
+
+    pub fn toggle_delete(&mut self) {
+        let i = self.list.get_index();
+        if i.is_none() {
+            return;
+        }
+
+        let i = i.unwrap();
+
+        self.list.items[i].toggle_delete();
+    }
+
+    pub fn get_entries_by<P>(&self, mut predicate: P) -> Vec<PathEntry>
+    where
+        P: FnMut(&PathEntry) -> bool,
+    {
+        self.list
+            .items
+            .iter()
+            .filter(|e| predicate(e))
+            .cloned()
+            .collect::<Vec<_>>()
+    }
+
+    pub fn delete_active_entries(&mut self) -> Result<(), ErrorBox> {
+        let entries_to_delete = self.get_entries_by(|e| e.is_delete());
+        for entry in entries_to_delete.iter() {
+            if entry.is_file {
+                fs::remove_file(entry.pathbuf.to_owned())?
+            } else {
+                fs::remove_dir_all(entry.pathbuf.to_owned())?
+            }
+        }
+
+        let entries = self.get_entries_by(|e| !e.is_delete());
+        self.update_list(entries);
+
+        Ok(())
+    }
+}
+#[derive(Clone)]
+pub struct PathEntry {
+    pub pathbuf: PathBuf,
+    pub is_file: bool,
+    _is_delete: bool,
+}
+
+impl PathEntry {
+    pub fn new(pathbuf: PathBuf) -> Self {
+        PathEntry {
+            is_file: pathbuf.is_file(),
+            pathbuf,
+            _is_delete: true,
+        }
+    }
+
+    pub fn toggle_delete(&mut self) {
+        self._is_delete = !self._is_delete;
+    }
+
+    pub fn is_delete(&self) -> bool {
+        self._is_delete
+    }
+}
+
+pub struct StatefulList<T> {
+    pub state: ListState,
+    pub items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    pub fn new() -> Self {
+        StatefulList {
+            state: ListState::default(),
+            items: vec![],
+        }
+    }
+
+    pub fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    pub fn get_index(&self) -> Option<usize> {
+        self.state.selected()
+    }
+
+    pub fn next(&mut self) {
+        if self.items.is_empty() {
+            return;
+        }
+
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        if self.items.is_empty() {
+            return;
+        }
+
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,287 @@
+use eradicate_tui::{App, ErrorBox, InputMode};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{
+    io,
+    time::{Duration, Instant},
+};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Corner, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, BorderType, Borders, List, ListItem, Paragraph},
+    Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
+
+fn main() -> Result<(), ErrorBox> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    let tick_rate = Duration::from_millis(250);
+
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app, tick_rate);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    app: &mut App,
+    tick_rate: Duration,
+) -> Result<(), ErrorBox> {
+    let mut last_tick = Instant::now();
+
+    loop {
+        terminal.draw(|f| draw_ui(f, app))?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                match app.input_mode {
+                    InputMode::Normal => match key.code {
+                        KeyCode::Enter => app.toggle_delete(),
+                        KeyCode::Down => app.list.next(),
+                        KeyCode::Up => app.list.previous(),
+                        KeyCode::Char('i') => app.toggle_case_sensitive(),
+                        KeyCode::Char('q') => break,
+                        KeyCode::Char('e') => {
+                            app.input_mode = InputMode::Insert;
+                        }
+                        KeyCode::Char('d') => app.delete_active_entries()?,
+                        _ => {}
+                    },
+                    InputMode::Insert => match key.code {
+                        KeyCode::Char(c) => {
+                            app.curret_input.push(c);
+                        }
+                        KeyCode::Enter => {
+                            // set the pattern
+                            let input = app.curret_input.clone();
+                            app.set_pattern(&input)?
+                        }
+                        KeyCode::Backspace => {
+                            app.curret_input.pop();
+                        }
+                        KeyCode::Esc => {
+                            app.input_mode = InputMode::Normal;
+                        }
+                        _ => {}
+                    },
+                }
+            }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            app.on_tick();
+            last_tick = Instant::now();
+        }
+    }
+
+    Ok(())
+}
+
+fn draw_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+        .split(f.size());
+
+    let bg_box = Block::default().style(Style::default().bg(Color::Black));
+
+    f.render_widget(bg_box, f.size());
+
+    // build left side
+    let left_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Percentage(5), Constraint::Percentage(95)].as_ref())
+        .split(chunks[0]);
+
+    let spans = Spans::from(vec![
+        Span::styled("[Enter]", Style::default().add_modifier(Modifier::BOLD)),
+        Span::raw(" to toggle entry deletion, "),
+        Span::styled("[d]", Style::default().add_modifier(Modifier::BOLD)),
+        Span::raw(" to delete active entries"),
+    ]);
+
+    let help_style = match app.input_mode {
+        InputMode::Normal => Style::default().add_modifier(Modifier::RAPID_BLINK),
+        InputMode::Insert => Style::default().fg(Color::Gray),
+    };
+
+    let mut text = Text::from(spans);
+    text.patch_style(help_style);
+    let help_text = Paragraph::new(text);
+    f.render_widget(help_text, left_chunks[0]);
+
+    let chunk_width = chunks[0].width as usize;
+
+    let items: Vec<ListItem> = app
+        .list
+        .items
+        .iter()
+        .map(|entry| {
+            let (turbo, turbo_color) = match entry.is_delete() {
+                true => ("o <> o", Color::Red),
+                false => ("- <> -", Color::Gray),
+            };
+
+            let file_type = if entry.is_file { "File" } else { "Dir" };
+
+            let header = Spans::from(vec![
+                Span::styled(file_type, Style::default().fg(Color::LightGreen)),
+                Span::raw(" "),
+                Span::styled(turbo, Style::default().fg(turbo_color)),
+            ]);
+
+            let path_display = entry.pathbuf.display().to_string();
+            let path_desc = Spans::from(vec![Span::raw(path_display)]);
+
+            ListItem::new(vec![
+                header,
+                path_desc,
+                Spans::from("-".repeat(chunk_width)),
+            ])
+            .style(Style::default().fg(Color::LightCyan).bg(Color::Black))
+        })
+        .collect();
+
+    let n = app.get_entries_by(|e| e.is_delete()).len();
+    let spans = Spans::from(vec![
+        Span::raw("Entries to eradicate: "),
+        Span::styled(
+            n.to_string(),
+            Style::default().add_modifier(Modifier::BOLD).fg(Color::Red),
+        ),
+        Span::raw(" "),
+    ]);
+
+    let list = List::new(items)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(spans.0)
+                .border_type(BorderType::Rounded),
+        )
+        .highlight_style(
+            Style::default()
+                .bg(Color::DarkGray)
+                .add_modifier(Modifier::ITALIC),
+        )
+        .start_corner(Corner::TopLeft);
+
+    f.render_stateful_widget(list, left_chunks[1], &mut app.list.state);
+
+    // build right side
+    let right_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Length(2),
+                Constraint::Length(3),
+                Constraint::Length(3),
+            ]
+            .as_ref(),
+        )
+        .split(chunks[1]);
+
+    // build help message
+
+    let (msg, style) = match app.input_mode {
+        InputMode::Normal => (
+            vec![
+                Span::styled("[e]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to start editing "),
+                Span::styled("[i]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to toogle case sensitive matches "),
+                Span::styled("[q]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to quit"),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        ),
+        InputMode::Insert => (
+            vec![
+                Span::styled("[Enter]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to set the pattern, "),
+                Span::styled("[Esc]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to stop editing"),
+            ],
+            Style::default(),
+        ),
+    };
+
+    let mut text = Text::from(Spans::from(msg));
+    text.patch_style(style);
+    let help_message = Paragraph::new(text);
+
+    f.render_widget(help_message, right_chunks[0]);
+
+    // display current pattern
+
+    let case_text = if app.is_case_sensitive() { "ON" } else { "OFF" };
+
+    let spans = match app.pattern.is_empty() {
+        false => Spans::from(vec![
+            Span::raw("Searching: "),
+            Span::styled(
+                app.pattern.as_str(),
+                Style::default().add_modifier(Modifier::BOLD),
+            ),
+            Span::raw(", case sensitive: "),
+            Span::styled(case_text, Style::default().add_modifier(Modifier::BOLD)),
+        ]),
+        true => Spans::from(vec![Span::styled(
+            "Empty pattern, try inserting a new one",
+            Style::default().add_modifier(Modifier::ITALIC),
+        )]),
+    };
+
+    let mut text = Text::from(spans);
+    text.patch_style(Style::default().fg(Color::Magenta));
+    f.render_widget(Paragraph::new(text), right_chunks[1]);
+
+    // display input
+
+    let input = Paragraph::new(app.curret_input.as_ref())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Insert => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Input"));
+
+    f.render_widget(input, right_chunks[2]);
+
+    match app.input_mode {
+        InputMode::Normal => {}
+        InputMode::Insert => f.set_cursor(
+            right_chunks[2].x + app.curret_input.width() as u16 + 1,
+            right_chunks[2].y + 1,
+        ),
+    }
+}
