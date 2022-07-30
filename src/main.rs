1abe368a53f7250fcdaf00e11137ebc1ef68aba6
Gers2017 <Gers2017@outlook.com>
2022-07-29 20:55:47 -0600

    Move input to the left and Input struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use eradicate_tui::{App, ErrorBox, AppMode};
+use eradicate_tui::{App, AppMode, ErrorBox};
 
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
@@ -76,17 +76,12 @@ fn run_app<B: Backend>(
                         _ => {}
                     },
                     AppMode::Insert => match key.code {
-                        KeyCode::Char(c) => {
-                            app.curret_input.push(c);
-                        }
+                        KeyCode::Char(ch) => app.push_ch(ch),
                         KeyCode::Enter => {
-                            let input = app.curret_input.clone();
-                            app.set_pattern(&input)?;
+                            app.set_pattern()?;
                             app.set_app_mode(AppMode::Normal);
                         }
-                        KeyCode::Backspace => {
-                            app.curret_input.pop();
-                        }
+                        KeyCode::Backspace => app.pop_ch(),
                         KeyCode::Esc => {
                             app.set_app_mode(AppMode::Normal);
                         }
@@ -97,7 +92,6 @@ fn run_app<B: Backend>(
         }
 
         if last_tick.elapsed() >= tick_rate {
-            app.on_tick();
             last_tick = Instant::now();
         }
     }
@@ -106,21 +100,117 @@ fn run_app<B: Backend>(
 }
 
 fn draw_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
-    let chunks = Layout::default()
+    let main_chunks = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
         .split(f.size());
 
     let bg_box = Block::default().style(Style::default().bg(Color::Black));
-
     f.render_widget(bg_box, f.size());
+    let left_area = main_chunks[0];
+    let right_area = main_chunks[1];
 
     // build left side
     let left_chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Length(1),
+                Constraint::Max(10),
+                Constraint::Max(10),
+            ]
+            .as_ref(),
+        )
+        .split(left_area);
+
+    // build help message
+
+    let (msg, style) = match app.app_mode {
+        AppMode::Normal => (
+            vec![
+                Span::styled("[i]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw("nsert mode"),
+                Span::raw(", "),
+                Span::styled("[g]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" toogle case sensitive matches, "),
+                Span::styled("[q]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw("uit"),
+            ],
+            Style::default(),
+        ),
+        AppMode::Insert => (
+            vec![
+                Span::styled("[Enter]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" set the pattern, "),
+                Span::styled("[Esc]", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" exit insert mode"),
+            ],
+            Style::default(),
+        ),
+    };
+
+    let mut text = Text::from(Spans::from(msg));
+    text.patch_style(style);
+    let help_message = Paragraph::new(text);
+
+    f.render_widget(help_message, left_chunks[0]);
+
+    // display current pattern
+
+    let case_text = if app.is_case_sensitive() { "ON" } else { "OFF" };
+
+    let spans = match app.pattern.content.is_empty() {
+        false => Spans::from(vec![
+            Span::raw("Searching: "),
+            Span::styled(
+                app.pattern.content.as_str(),
+                Style::default().add_modifier(Modifier::BOLD),
+            ),
+            Span::raw(", case sensitive: "),
+            Span::styled(case_text, Style::default().add_modifier(Modifier::BOLD)),
+            Span::raw(", "),
+        ]),
+        true => Spans::from(vec![Span::styled(
+            "Empty pattern, try inserting a new one",
+            Style::default().add_modifier(Modifier::ITALIC),
+        )]),
+    };
+
+    let mut text = Text::from(spans);
+    text.patch_style(Style::default().fg(Color::Magenta));
+    f.render_widget(Paragraph::new(text), left_chunks[1]);
+
+    // display input
+
+    let (name, content) = (&app.pattern.name, &app.pattern.content);
+    let style = match app.app_mode {
+        AppMode::Insert => app.pattern.active_style,
+        AppMode::Normal => app.pattern.normal_style,
+    };
+    
+    let pattern_input = create_input(name, content, style);
+    f.render_widget(pattern_input, left_chunks[2]);
+
+    let active_area = left_chunks[2];
+
+    match app.app_mode {
+        AppMode::Normal => {}
+        AppMode::Insert => f.set_cursor(
+            active_area.x + app.pattern.content.width() as u16 + 1,
+            active_area.y + 1,
+        ),
+    }
+
+    // end build left side
+
+    // build right side
+    let right_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
         .constraints([Constraint::Percentage(5), Constraint::Percentage(95)].as_ref())
-        .split(chunks[0]);
+        .split(right_area);
 
     let spans = Spans::from(vec![
         Span::styled("[Enter]", Style::default().add_modifier(Modifier::BOLD)),
@@ -137,9 +227,9 @@ fn draw_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let mut text = Text::from(spans);
     text.patch_style(help_style);
     let help_text = Paragraph::new(text);
-    f.render_widget(help_text, left_chunks[0]);
+    f.render_widget(help_text, right_chunks[0]);
 
-    let chunk_width = chunks[0].width as usize;
+    let chunk_width = right_area.width as usize;
 
     let items: Vec<ListItem> = app
         .list
@@ -195,93 +285,14 @@ fn draw_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         )
         .start_corner(Corner::TopLeft);
 
-    f.render_stateful_widget(list, left_chunks[1], &mut app.list.state);
+    f.render_stateful_widget(list, right_chunks[1], &mut app.list.state);
+}
 
-    // build right side
-    let right_chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(1)
-        .constraints(
-            [
-                Constraint::Length(2),
-                Constraint::Length(3),
-                Constraint::Length(3),
-            ]
-            .as_ref(),
-        )
-        .split(chunks[1]);
-
-    // build help message
-
-    let (msg, style) = match app.app_mode {
-        AppMode::Normal => (
-            vec![
-                Span::styled("[i]", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw("nsert mode "),
-                Span::styled("[g]", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" toogle case sensitive matches "),
-                Span::styled("[q]", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw("uit"),
-            ],
-            Style::default(),
-        ),
-        AppMode::Insert => (
-            vec![
-                Span::styled("[Enter]", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" set the pattern, "),
-                Span::styled("[Esc]", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" exit insert mode"),
-            ],
-            Style::default(),
-        ),
-    };
-
-    let mut text = Text::from(Spans::from(msg));
-    text.patch_style(style);
-    let help_message = Paragraph::new(text);
-
-    f.render_widget(help_message, right_chunks[0]);
-
-    // display current pattern
-
-    let case_text = if app.is_case_sensitive() { "ON" } else { "OFF" };
-
-    let spans = match app.pattern.is_empty() {
-        false => Spans::from(vec![
-            Span::raw("Searching: "),
-            Span::styled(
-                app.pattern.as_str(),
-                Style::default().add_modifier(Modifier::BOLD),
-            ),
-            Span::raw(", case sensitive: "),
-            Span::styled(case_text, Style::default().add_modifier(Modifier::BOLD)),
-        ]),
-        true => Spans::from(vec![Span::styled(
-            "Empty pattern, try inserting a new one",
-            Style::default().add_modifier(Modifier::ITALIC),
-        )]),
-    };
-
-    let mut text = Text::from(spans);
-    text.patch_style(Style::default().fg(Color::Magenta));
-    f.render_widget(Paragraph::new(text), right_chunks[1]);
-
-    // display input
-
-    let input = Paragraph::new(app.curret_input.as_ref())
-        .style(match app.app_mode {
-            AppMode::Normal => Style::default(),
-            AppMode::Insert => Style::default().fg(Color::Yellow),
-        })
-        .block(Block::default().borders(Borders::ALL).border_type(BorderType::Rounded).title("Pattern"));
-
-    f.render_widget(input, right_chunks[2]);
-
-    match app.app_mode {
-        AppMode::Normal => {}
-        AppMode::Insert => f.set_cursor(
-            right_chunks[2].x + app.curret_input.width() as u16 + 1,
-            right_chunks[2].y + 1,
-        ),
-    }
+fn create_input<'a>(name: &'a str, text: &'a str, style: Style) -> Paragraph<'a> {
+    Paragraph::new(text).style(style).block(
+        Block::default()
+            .borders(Borders::ALL)
+            .border_type(BorderType::Rounded)
+            .title(name),
+    )
 }
